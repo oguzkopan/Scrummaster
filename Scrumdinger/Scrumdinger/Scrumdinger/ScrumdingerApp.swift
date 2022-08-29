d4b24bfd5ec262c428a4fe41db920d84198c6cc6
oguzkopan <oguzkopan@gmail.com>
2022-08-29 13:43:17 +0300

    Add files via upload
    MeetingView File is Created

---
+++ Scrumdinger/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Oğuz Kopan on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
